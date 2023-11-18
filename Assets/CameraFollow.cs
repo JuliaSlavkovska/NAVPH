df42a8318a9d1e41fc1bce43700c9c0cd7df3757
JuliaSlavkovska <55793090+JuliaSlavkovska@users.noreply.github.com>
2023-11-18 21:01:03 +0100

    view rotation limitation

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,15 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    Vector2 rotation = Vector2.zero;
-    [SerializeField] private float speed = 3;
+    [SerializeField] private float speed;
+    [SerializeField] private float minAngle;
+    [SerializeField] private float maxAngle;
+    float yRotate = 0.0f;
 
+    void Start () {
+        speed = 3;
+        minAngle = -80.0f;
+        maxAngle = 60.0f;
+    }
+    
     void Update () {
-        rotation.y += Input.GetAxis ("Mouse X");
-        transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, rotation.y * speed, transform.eulerAngles.z);
-        //transform.eulerAngles = (Vector2)rotation * speed;
+        yRotate += Input.GetAxis ("Mouse X") * speed;
+        yRotate = Mathf.Clamp (yRotate, minAngle, maxAngle);
+        transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, yRotate, transform.eulerAngles.z);
     }
 }
