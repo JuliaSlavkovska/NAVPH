61eafb1555079ecd352a60223806163aab4cf9f0
JuliaSlavkovska <55793090+JuliaSlavkovska@users.noreply.github.com>
2023-12-09 23:40:58 +0100

    Building Layout

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,11 +10,15 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float maxAngle;
     float yRotate = 0.0f;
     private bool freeze = false;
+    private float eulerx; 
+    private float eulerz; 
 
     void Start () {
         speed = 3;
         minAngle = -80.0f;
         maxAngle = 60.0f;
+        eulerx = transform.eulerAngles.x;
+        eulerz = transform.eulerAngles.z;
     }
 
     void Update()
@@ -23,7 +27,7 @@ public class CameraFollow : MonoBehaviour
         {
             yRotate += Input.GetAxis("Mouse X") * speed;
             yRotate = Mathf.Clamp(yRotate, minAngle, maxAngle);
-            transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, yRotate, transform.eulerAngles.z);
+            transform.localRotation = Quaternion.Euler(eulerx, yRotate, eulerz);
         }
         else
         {
